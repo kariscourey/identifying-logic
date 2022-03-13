060f74ff9c982fa9575c880066c33a63bda34238
Daniel Billotte <daniel.billotte@galvanize.com>
2022-03-13 10:36:58 -0700

    initial commit

---
+++ tests/test_ifs.py
@@ -0,0 +1,7 @@
+from problems.identifying_ifs1 import *
+from problems.identifying_ifs2 import *
+from problems.identifying_ifs3 import *
+
+def test_func1():
+    input = 5
+    assert echo(input) == input
